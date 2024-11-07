35565943cfd1c964fd03954af51e5c969fad114a
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-11-07 11:39:43 +0900

    最低限のネットワークの疎通完了

--- 02_emulator/bin/02_emulator.ts
+++ 02_emulator/bin/02_emulator.ts
@@ -4,6 +4,13 @@ import { EmulatorStack } from '../lib/02_emulator-stack';
 
 const app = new cdk.App();
 new EmulatorStack(app, 'EmulatorStack', {
+    region: 'ap-northeast-1',
     instanceAmiId: 'ami-0eba6c58b7918d3a1',
-    region: 'ap-northeast-1'
+    hostedZoneId: 'Z036663027HMHLRMORQ6C',
+    domainName: 'emulator.bam-b-00.com',
+    acmArn: 'arn:aws:acm:ap-northeast-1:594175341170:certificate/1a4791b7-c128-41ef-9b8f-d8de2ded93b5',
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
 });
--- 02_emulator/lib/02_emulator-stack.ts
+++ 02_emulator/lib/02_emulator-stack.ts
@@ -1,11 +1,19 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as elbv2tg from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53Tg from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import { Construct } from 'constructs';
 
 interface EmulatorStackProps extends cdk.StackProps {
-  instanceAmiId: string
   region: string
+  instanceAmiId: string
+  domainName: string
+  hostedZoneId: string
+  acmArn: string
 }
 
 export class EmulatorStack extends cdk.Stack {
@@ -28,9 +36,9 @@ export class EmulatorStack extends cdk.Stack {
     // --------------------
     // Security Group
     // --------------------
-    const securityGroupEc2 = new ec2.SecurityGroup(this, 'SecurityGroup', {
+    const securityGroupEc2 = new ec2.SecurityGroup(this, 'SecurityGroupEc2', {
       vpc,
-      description: 'IBC LPC Node',
+      description: 'EC2 SG for Android Emulator',
       allowAllOutbound: true
     });
 
@@ -44,6 +52,54 @@ export class EmulatorStack extends cdk.Stack {
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       'allow HTTP traffic from anywhere',
+    );
+
+    securityGroupEc2.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(50051),
+      'allow gRPC traffic from anywhere',
+    );
+
+    securityGroupEc2.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8080),
+      'allow gRPC traffic from anywhere',
+    );
+
+    const securityGroupAlb = new ec2.SecurityGroup(this, 'SecurityGroupAlb', {
+      vpc,
+      description: 'ALB SG for Android Emulator',
+      allowAllOutbound: true
+    });
+
+    securityGroupAlb.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    securityGroupAlb.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    securityGroupAlb.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(50051),
+      'allow gRPC traffic from anywhere',
+    );
+
+    securityGroupAlb.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8080),
+      'allow gRPC traffic from anywhere',
+    );
+
+    securityGroupAlb.addEgressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.allTraffic(),
+      'allow all traffic',
     );
 
     // --------------------
@@ -78,5 +134,79 @@ export class EmulatorStack extends cdk.Stack {
           },
         ],
     });
+
+    // --------------------
+    // ALB
+    // --------------------
+    // ACM
+    const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', props?.acmArn as string);
+    const alb = new elbv2.ApplicationLoadBalancer(this, "ALB", {
+      vpc: vpc,
+      vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+      internetFacing: true,
+      securityGroup: securityGroupAlb,
+    });
+
+    // Target Group
+    const listener443 = alb.addListener("listener443", { 
+      port: 443,
+      certificates: [certificate]
+    });
+    listener443.addTargets('target443', {
+      port: 80,
+      targets: [new elbv2tg.InstanceIdTarget(instance.instanceId)],
+      healthCheck: {
+        port: '80',
+        protocol: elbv2.Protocol.HTTP,
+        path: '/',
+        interval: cdk.Duration.seconds(10),
+        healthyThresholdCount: 2
+      }
+    });
+
+    const listener8080 = alb.addListener("listener8080", { 
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      port: 8080,
+      certificates: [certificate]
+    });
+    listener8080.addTargets('target8080HttpPost', {
+      port: 8080,
+      targets: [new elbv2tg.InstanceIdTarget(instance.instanceId)],
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      protocolVersion: elbv2.ApplicationProtocolVersion.GRPC,
+      healthCheck: {
+        interval: cdk.Duration.seconds(10),
+        port: '8080',
+        path: '/android.emulation.control.EmulatorController/getStatus',
+        healthyGrpcCodes: '0-99'
+      }
+    });
+    listener8080.addTargets('target8080HttpOptions', {
+      port: 8080,
+      priority: 10,
+      conditions: [
+        elbv2.ListenerCondition.httpRequestMethods(['OPTIONS']),
+      ],
+      targets: [new elbv2tg.InstanceIdTarget(instance.instanceId)],
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      protocolVersion: elbv2.ApplicationProtocolVersion.HTTP2,
+      healthCheck: {
+        interval: cdk.Duration.seconds(10),
+        port: '80',
+        path: '/',
+      }
+    });
+
+    // Route53 A-Record
+    // new route53.ARecord(this, 'AlbAliasRecord', {
+    //   recordName: props?.domainName as string,
+    //   zone: new route53.PrivateHostedZone()
+    //   // zone: new route53.HostedZone(this, 'EmulatorHostedZone', {
+    //   //   zoneName: 'bam-b-00.com'
+    //   // }),
+    //   target: route53.RecordTarget.fromAlias(
+    //     new route53Tg.LoadBalancerTarget(alb)
+    //   ),
+    // })
   }
 }
